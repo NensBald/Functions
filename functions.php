f04a0690b6636ba847c0e37258411004f317b5fd
narendrabaldha <narendrabaldha87@gmail.com>
2018-10-15 16:50:05 +0530

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,9 @@
+function isEmpty($str) {
+    $check = trim($str);
+    return (empty($check) && strlen($check) == 0) ? true : false;
+}
+
+function isValidEmail($string) {
+    $validate = preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", $string);
+    return (!$validate) ? true : false;
+}
