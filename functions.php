82b3be0378e5cae0e56ba923774fa9a84398fecf
Das Na Das <Das Na Das@DESKTOP-MRI0TSR>
2018-10-16 15:09:11 +0530

    create 'isInt' function

--- functions.php
+++ functions.php
@@ -1,11 +1,17 @@
 <?php
+
 function isEmpty($str) {
-    $check = trim($str);
-    return (empty($check) && strlen($check) == 0) ? true : false;
+        $check = trim($str);
+        return (empty($check) && strlen($check) == 0) ? true : false;
 }
 
 function isValidEmail($string) {
-    $validate = preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", $string);
-    return (!$validate) ? true : false;
+        $validate = preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", $string);
+        return (!$validate) ? true : false;
 }
+
+function isInt($int) {
+        return (!filter_var($int, FILTER_VALIDATE_INT) === false) ? true : false;
+}
+
 ?>
